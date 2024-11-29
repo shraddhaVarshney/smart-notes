32145142e37c1510c48f32b211fd20db6a19611e
shivam-1214 <er.shivam1214@gmail.com>
2024-11-29 20:41:08 +0530

    committed

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,44 @@
-import Image from "next/image";
+"use client"
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+
+import { FaMicrophoneSlash } from "react-icons/fa";
+import { FaMicrophone } from "react-icons/fa";
+
+import { MdDownload } from "react-icons/md";
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [micOn,setMicOn] = useState(false);
+  const [notes,setNotes] = useState<String>("something");
+
+  const toggleMic =()=>{
+    setMicOn(prev=>!prev);
+
+    // const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    // recognition.lang = 'en-US';
+    // recognition.start();
+
+  }
+
+  return (
+    <div className="w-full flex justify-center gap-2 items-center p-5 ">
+      <div className="w-full flex justify-center items-center px-3 h-[90vh] flex-col gap-2 rounded-lg bg-[#1b1b1b]">
+    {/* <h1 className="text-3xl font-semibold">Smart Notes</h1> */}
+    <Button onClick={()=>toggleMic()} className="flex gap-2 w-full">
+      {micOn ? <FaMicrophoneSlash size={45}/> :<FaMicrophone size={45}/>}
+      Speak</Button>
+    <Textarea className="rounded-md border-none bg-[#141414]" placeholder="type your notes here..." rows={20}/>
     </div>
+    <div className="w-full h-[90vh] bg-[#1b1b1b] p-3 rounded-lg flex flex-col gap-2" id="notesDisplay ">
+      <div className="w-full flex items-center justify-center p-3 font-bold text-[20px]">Your Notes</div>
+      <div className="w-full flex-1 bg-[#141414] p-3">{notes}</div>
+      <Button className="flex gap-2 justify-center items-center"><MdDownload size={45}/> Download Notes</Button>
+    </div>
+</div>
+
   );
 }
